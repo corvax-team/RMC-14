d8ada2ad125a227f068d6e7c37f91ab7f3da7a1c
AsnDen <asnden@gmail.com>
2024-07-19 17:25:46 +0300

    Revert "Revert "Адаптация апстрима""
    This reverts commit 08d63454f6e62120dd54b42baf6701b0a4681be3.

--- Content.Server/_RMC14/Rules/CMDistressSignalRuleSystem.cs
+++ Content.Server/_RMC14/Rules/CMDistressSignalRuleSystem.cs
@@ -107,8 +107,9 @@ public sealed class CMDistressSignalRuleSystem : GameRuleSystem<CMDistressSignal
     [ViewVariables]
     public readonly Dictionary<string, float> MarinesPerXeno = new()
     {
-        ["/Maps/_RMC14/lv624.yml"] = 3.75f,
-        ["/Maps/_RMC14/solaris.yml"] = 4.75f,
+//        ["/Maps/_RMC14/lv624.yml"] = 3.75f,
+//        ["/Maps/_RMC14/solaris.yml"] = 4.75f,
+        ["Maps/_CCM14/Planets/lv624.yml"] = 3.75f // CCM
     };
 
     private readonly List<MapId> _almayerMaps = [];
