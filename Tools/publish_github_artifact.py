12d59c8343f4854dde8f095e55b9be4fd96a1665
YesImMorty <164863999+YesImMorty@users.noreply.github.com>
2024-07-27 15:22:17 +0300

    Update publish_github_artifact.py

--- Tools/publish_github_artifact.py
+++ Tools/publish_github_artifact.py
@@ -14,8 +14,8 @@ VERSION = os.environ['GITHUB_SHA']
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://wizards.cdn.spacestation14.com/"
-FORK_ID = "wizards"
+ROBUST_CDN_URL = "https://cdn.station14.ru/"
+FORK_ID = "colonialmarines"
 
 def main():
     print("Fetching artifact URL from API...")
@@ -38,7 +38,8 @@ def main():
 def get_artifact_url() -> str:
     headers = {
         "Authorization": f"Bearer {GITHUB_TOKEN}",
-        "X-GitHub-Api-Version": "2022-11-28"
+        "X-GitHub-Api-Version": "2022-11-28",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
     resp = requests.get(f"https://api.github.com/repos/{GITHUB_REPOSITORY}/actions/artifacts/{ARTIFACT_ID}/zip", allow_redirects=False, headers=headers)
     resp.raise_for_status()
