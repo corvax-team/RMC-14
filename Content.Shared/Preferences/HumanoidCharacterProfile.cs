87d611bf4a2b0f9de56365b566eeddd36e85ee6b
AsnDen <asnden@gmail.com>
2024-08-20 03:02:20 +0300

    Revert "Localization of names (#40)"
    This reverts commit e54b9892c19b9b91b9f9a96dcd76d5b74a5677d9.

--- Content.Shared/Preferences/HumanoidCharacterProfile.cs
+++ Content.Shared/Preferences/HumanoidCharacterProfile.cs
@@ -581,7 +581,7 @@ namespace Content.Shared.Preferences
                 flavortext = FormattedMessage.RemoveMarkup(FlavorText);
             }
             //sponsorPrototypes.ToArray()
-            var appearance = HumanoidCharacterAppearance.EnsureValid(Appearance, Species, Sex);
+            var appearance = HumanoidCharacterAppearance.EnsureValid(Appearance, Species, Sex, sponsorPrototypes.ToArray());
 
             var prefsUnavailableMode = PreferenceUnavailable switch
             {
---
+++ Resources/Locale/ru-RU/_RCM14/armor/rmc-bulky-armor.ftl
@@ -0,0 +1,2 @@
+﻿rmc-bulky-armor-target-unable = {$target} Недостаёт силы носить её {$armor}!
+rmc-bulky-armor-user-unable = Нехватает силушек что-б носить {$armor}!
