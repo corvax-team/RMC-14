c58bf8d771dfa2b2bb02247568e739cc35c4b8e3
KirDefuse <vibrik0@gmail.com>
2025-05-26 10:09:02 +0300

    Нет админ абузу

--- Content.Server/Administration/Commands/SetOutfitCommand.cs
+++ Content.Server/Administration/Commands/SetOutfitCommand.cs
@@ -19,7 +19,7 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Server.Administration.Commands
 {
-    [AdminCommand(AdminFlags.Admin)]
+    [AdminCommand(AdminFlags.Fun)] // CCM ToDo
     public sealed class SetOutfitCommand : IConsoleCommand
     {
         [Dependency] private readonly IEntityManager _entities = default!;
