22ec0ce1109a6d5c59f4656b70aef6f3273bb9f9
VoRoneh <898797gsdg@mail.ru>
2025-12-20 21:11:38 +0300

    fixes

---
+++ Content.Shared/_RMC14/Xenonids/Egg/CCMXenoRoyalEggComponent.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Xenonids.Egg;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(XenoEggSystem))]
+public sealed partial class CCMXenoRoyalEggComponent : Component
+{
+}
