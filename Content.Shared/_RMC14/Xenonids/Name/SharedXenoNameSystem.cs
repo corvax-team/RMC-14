0edcc874945b441703dac53bab03505c9627a2b1
Fenriz <kastonbag552@gmail.com>
2026-03-11 15:43:44 +0200

    fix2

--- Content.Shared/_RMC14/Xenonids/Name/SharedXenoNameSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Name/SharedXenoNameSystem.cs
@@ -131,7 +131,7 @@ public abstract class SharedXenoNameSystem : EntitySystem
         }
         catch (Exception e)
         {
-            Log.Warning($"Error reading total xeno playtime:\n{e}");
+            Log.Error($"Error reading total xeno playtime:\n{e}");
         }
 
         return xenoPlaytime;
