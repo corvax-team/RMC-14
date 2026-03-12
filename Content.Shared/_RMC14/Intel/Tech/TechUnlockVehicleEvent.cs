b32466fc09e88e87a426c1ac00d6519ea33f362c
Fenriz <kastonbag552@gmail.com>
2026-03-12 04:34:47 +0200

    fix9.5

--- Content.Shared/_RMC14/Intel/Tech/TechUnlockVehicleEvent.cs
+++ Content.Shared/_RMC14/Intel/Tech/TechUnlockVehicleEvent.cs
@@ -4,4 +4,4 @@ using Robust.Shared.Serialization;
 namespace Content.Shared._RMC14.Intel.Tech;
 
 [Serializable, NetSerializable]
-public sealed record TechUnlockVehicleEvent(string unlock);
+public sealed record TechUnlockVehicleEvent(string Unlock);
