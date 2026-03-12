399ddf2787abbad99e6d1770f022a2a6f48f1470
Fenriz <kastonbag552@gmail.com>
2026-03-12 04:33:31 +0200

    fix9.4

--- Content.Shared/_RMC14/Intel/Tech/TechUnlockVehicleEvent.cs
+++ Content.Shared/_RMC14/Intel/Tech/TechUnlockVehicleEvent.cs
@@ -4,8 +4,4 @@ using Robust.Shared.Serialization;
 namespace Content.Shared._RMC14.Intel.Tech;
 
 [Serializable, NetSerializable]
-public sealed record TechUnlockVehicleEvent
-{
-    [DataField("unlock")]
-    public string Unlock = null!;
-}
+public sealed record TechUnlockVehicleEvent(string unlock);
