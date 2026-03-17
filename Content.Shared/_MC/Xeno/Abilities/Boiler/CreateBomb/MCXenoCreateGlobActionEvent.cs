2c7ae1bf56a2cde40823d00aa5ab4bc6b2387ce4
KirDefuse <vibrik0@gmail.com>
2026-03-18 00:09:24 +0200

    Revert "feat: create bomb boiler ability"
    This reverts commit 0d230d9a2227fde3c5fad6c39c3063071ee45c84.

--- Content.Shared/_MC/Xeno/Abilities/Boiler/CreateBomb/MCXenoCreateGlobActionEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Content.Shared.Actions;
-
-namespace Content.Shared._MC.Xeno.Abilities.Boiler.CreateBomb;
-
-public sealed partial class MCXenoCreateGlobActionEvent : InstantActionEvent
-{
-    [DataField]
-    public string StatePrefix = "create_bomb_count_";
-}
