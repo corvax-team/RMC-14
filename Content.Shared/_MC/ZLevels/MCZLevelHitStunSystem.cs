1f130a77b57dbe40de5d5b07406a8a7eaffac20e
KirDefuse <vibrik0@gmail.com>
2026-03-21 21:27:31 +0200

    revert

--- Content.Shared/_MC/ZLevels/MCZLevelHitStunSystem.cs
+++ Content.Shared/_MC/ZLevels/MCZLevelHitStunSystem.cs
@@ -2,7 +2,7 @@ using Content.Shared._CE.ZLevels.Core.EntitySystems;
 
 namespace Content.Shared._MC.ZLevels;
 
-public sealed class MCZLevelFallStunSystem : EntitySystem
+public sealed class MCZLevelHitStunSystem : EntitySystem
 {
     [Dependency] private readonly SharedStunSystem _stun = default!;
 
@@ -10,10 +10,10 @@ public sealed class MCZLevelFallStunSystem : EntitySystem
     {
         base.Initialize();
 
-        SubscribeLocalEvent<MCZLevelFallStunComponent, CEZLevelHitEvent>(OnHit);
+        SubscribeLocalEvent<MCZLevelHitStunComponent, CEZLevelHitEvent>(OnHit);
     }
 
-    private void OnHit(Entity<MCZLevelFallStunComponent> entity, ref CEZLevelHitEvent args)
+    private void OnHit(Entity<MCZLevelHitStunComponent> entity, ref CEZLevelHitEvent args)
     {
         _stun.TryStun(target, TimeSpan.FromSeconds(1f), true);
         _stun.TryKnockdown(target, TimeSpan.FromSeconds(1f), true);
