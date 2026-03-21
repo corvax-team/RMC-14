67b98547f7b4153ce1ede7e9b6b6cad99984795d
KirDefuse <vibrik0@gmail.com>
2026-03-21 21:26:49 +0200

    Revert "Fix Zlevel"
    This reverts commit 55cdbc9f302f8022a8e396166bd34a47762f04b3.

--- Content.Shared/_MC/ZLevels/MCZLevelHitStunSystem.cs
+++ Content.Shared/_MC/ZLevels/MCZLevelHitStunSystem.cs
@@ -1,5 +1,4 @@
 using Content.Shared._CE.ZLevels.Core.EntitySystems;
-using Content.Shared.Stunnable;
 
 namespace Content.Shared._MC.ZLevels;
 
@@ -16,7 +15,7 @@ public sealed class MCZLevelFallStunSystem : EntitySystem
 
     private void OnHit(Entity<MCZLevelFallStunComponent> entity, ref CEZLevelHitEvent args)
     {
-    _stun.TryStun(entity, TimeSpan.FromSeconds(1f), true);
-    _stun.TryKnockdown(entity, TimeSpan.FromSeconds(1f), true);
+        _stun.TryStun(target, TimeSpan.FromSeconds(1f), true);
+        _stun.TryKnockdown(target, TimeSpan.FromSeconds(1f), true);
     }
 }
