67df09567cb88cd9e8191d1605cd456c700dceba
KirDefuse <vibrik0@gmail.com>
2026-03-21 04:30:19 +0200

    Add MC weapon system

---
+++ Content.Shared/_MC/Weapon/MCWeaponFireDelayComponent.cs
@@ -0,0 +1,13 @@
+﻿using Content.Shared.DoAfter;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._MC.Weapon;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class MCWeaponFireDelayComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public TimeSpan Delay = TimeSpan.FromSeconds(1);
+
+    public DoAfterId? DoAfterId;
+}
