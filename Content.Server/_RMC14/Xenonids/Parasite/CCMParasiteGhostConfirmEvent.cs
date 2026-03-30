5d41e7a91658966d630388582f75184d93f3423e
Fenriz <kastonbag552@gmail.com>
2026-03-30 19:16:08 +0300

    упс

--- Content.Server/_RMC14/Xenonids/Parasite/CCMParasiteGhostConfirmEvent.cs
+++
@@ -1,8 +0,0 @@
-using Robust.Shared.Serialization;
-
-namespace Content.Server._RMC14.Xenonids.Parasite;
-
-public sealed record class CCMParasiteGhostConfirmEvent
-{
-    public int ConfirmId { get; set; }
-}
