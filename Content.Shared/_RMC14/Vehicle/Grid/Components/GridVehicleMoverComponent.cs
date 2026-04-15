94d91158a2969d9b7f2adb06a85b0848ed8b89ce
Vixting <vixting@gmail.com>
2026-04-15 15:11:56 +0100

    get out of the way

--- Content.Shared/_RMC14/Vehicle/Grid/Components/GridVehicleMoverComponent.cs
+++ Content.Shared/_RMC14/Vehicle/Grid/Components/GridVehicleMoverComponent.cs
@@ -113,6 +113,9 @@ public sealed partial class GridVehicleMoverComponent : Component
     [DataField, AutoNetworkedField]
     public RMCSizes? XenoPushMinimumSize;
 
+    [DataField, AutoNetworkedField]
+    public bool CanPushVehicles = false;
+
     [NonSerialized]
     public EntityUid? SyncedGrid;
 
