c7d165b6d55298abfcd615efac1deb2d7bffba68
KirDefuse <vibrik0@gmail.com>
2026-04-17 02:05:14 +0300

    Revert "runi (nado fix)"
    This reverts commit 9c442bf065a422aaf4022135b29228533b56c95c.

--- Content.Shared/_CCM/Xenonids/Abilities/Runi/CCMLeapEvent.cs
+++
@@ -1,7 +0,0 @@
-using Content.Shared.Actions;
-using Robust.Shared.Serialization;
-
-namespace Content.Shared._CCM.Xeno.Abilities.ZigZagPounce;
-
-[Serializable, NetSerializable]
-public sealed partial class CCMZigZagPounceActionEvent : WorldTargetActionEvent;
